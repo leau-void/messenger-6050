e1905cb9376aa6818d3cbddc2ae3b48874ea5f8c
leau-void <lpierra@gmail.com>
2021-11-21 11:08:39 -0400

    change postMessage to handle returned Promise asynchronously

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -94,19 +94,15 @@ const sendMessage = (data, body) => {
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
 export const postMessage = (body) => (dispatch) => {
-  try {
-    const data = saveMessage(body);
-
+  saveMessage(body).then((data) => {
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
       dispatch(setNewMessage(data.message));
     }
-
+    
     sendMessage(data, body);
-  } catch (error) {
-    console.error(error);
-  }
+  }).catch((error) => console.error(error))
 };
 
 export const searchUsers = (searchTerm) => async (dispatch) => {
