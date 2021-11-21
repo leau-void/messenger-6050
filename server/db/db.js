141f0dda8750974f9c2b098cab3de5b3afd4bb83
leau-void <lpierra@gmail.com>
2021-11-21 10:57:58 -0400

    add connection to db through Sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    "postgres://leau:leausdbpassword@localhost:5432/messenger",
+  {
+    logging: false,
+    dialect: "postgres",
+  }
+);
 
 module.exports = db;
