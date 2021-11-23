08eda25129e15d0cb42cbfbd001c03fa00089e38
leau-void <lpierra@gmail.com>
2021-11-23 17:25:38 -0400

    add background image & setup backgrounds sidebar

--- client/src/components/AuthenticationPage/Sidebar.js
+++ client/src/components/AuthenticationPage/Sidebar.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import authpageSidebarImage from "../../assets/authpageSidebarImage.png"
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "100%",
+    height: "100vh",
+    width: "40vw",
+    padding: 0,
+    background: `top / contain no-repeat url(${authpageSidebarImage})`,
+    position: "relative",
   },
+  content: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    background: "linear-gradient(rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85))"
+  }
 }));
 
 const Sidebar = (props) => {
+  const classes = useStyles();
   return (
-    <Box>
-      <img src="" alt="Background" />
+    <Box className={classes.root}>
+      <Box className={classes.content}>
+      </Box>
     </Box>
   )
 };
