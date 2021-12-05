a3e10b046106d1ffa477c0af0b716f39ffd1351d
leau-void <lpierra@gmail.com>
2021-12-05 16:49:16 -0400

    fix db setup file to use .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,7 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    "postgres://leau:leausdbpassword@localhost:5432/messenger",
+  process.env.DATABASE_URL,
   {
     logging: false,
     dialect: "postgres",
